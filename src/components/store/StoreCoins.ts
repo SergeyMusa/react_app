4f4e97c120e589f3734f08d9e4bfbb3f51f6459a
MuSa <78234427+SergeyMusa@users.noreply.github.com>
2023-04-04 18:11:21 +0300

    wip - begin modal in tables

--- src/components/store/StoreCoins.ts
+++ src/components/store/StoreCoins.ts
@@ -5,7 +5,7 @@ class StoreCoins {
   modalId = 0
   public IsLoading: boolean = true
   // public FetchUrl = `https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD`
-  public FetchUrl = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD`
+  public FetchUrl = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD&api_key={be8e1361d9e8e83cbdd0723b8e3792826ef753bbf8f8813ed8cf5d34dae7d07e}`
   PreData: [] = []
   ItemData: any
   searchItems = []
