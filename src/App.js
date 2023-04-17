2a014ac60c82094e496d206b9ddd639df4af3438
MuSa <78234427+SergeyMusa@users.noreply.github.com>
2023-04-17 16:24:58 +0300

    edit css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import Button from '@mui/material/Button';
-import './assets/styles/main.scss'
+import './assets/styles/main.css'
 import {Container} from "@mui/material";
 import {CardListScreen} from "_view/screen/CardList/CardList.screen";
 import {Home} from "_view/screen/Home/Home";
 import {TablesListScreen} from "_view/screen/Table/TablesList.screen";
 import {Header} from "_view/components/Header/Header";
 import Footer from "_view/components/Footer/Footer";
-import {Timer} from "_view/components/Timer/Timer";
+// import {Timer} from "_view/components/Timer/Timer";
 
 function App() {
   return (
