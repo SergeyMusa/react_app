4858582734f419652b7edd731f98c66c27c30103
MuSa <78234427+SergeyMusa@users.noreply.github.com>
2023-04-19 14:18:50 +0300

    add plug json if err load

--- src/common/const/Const.ts
+++ src/common/const/Const.ts
@@ -1,2 +1,3 @@
 
-export const URL_COINS = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD&api_key={be8e1361d9e8e83cbdd0723b8e3792826ef753bbf8f8813ed8cf5d34dae7d07e}`
+export const URL_COINS = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD&api_key={be8e1361d9e8e83cbdd0723b8e3792826ef753bbf8f8813ed8cf5d34dae7d07e}`;
+export const PLUG_JSON = '/src/assets/json/plug.json';
